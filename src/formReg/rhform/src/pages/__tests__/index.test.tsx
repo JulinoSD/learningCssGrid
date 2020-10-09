7278a2adf7a97b3cc7e3ff331259221ce8e7a864
Julino Segunda Dídimo <ddimojulino@gmail.com>
2020-10-09 20:25:20 -0300

    Learning

---
+++ src/formReg/rhform/src/pages/__tests__/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render, fireEvent, act } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Home from '../index'
+
+test("it should validate length", async () => {
+    const { findByLabelText, findByText, findByRole } = render(<Home />);
+    const input = await findByLabelText("Password");
+
+    await act(async () => {
+        fireEvent.input(input, { target: { value: "abcd" } })
+        fireEvent.submit(await findByRole("button"))
+
+        const error = await findByText('must be 8 chars')
+        expect(error).toBeInTheDocument()
+    })
+})
+test("it shold validate complexity", async () => {
+    const { findByLabelText, findByText, findByRole } = render(<Home />);
+    const input = await findByLabelText("Password");
+
+    await act(async () => {
+        fireEvent.input(input, { target: { value: "abcdefghdf" } })
+        fireEvent.submit(await findByRole("button"))
+
+        const error = await findByText(/Must include uppercase/)
+        expect(error).toBeInTheDocument()
+    })
+ })
